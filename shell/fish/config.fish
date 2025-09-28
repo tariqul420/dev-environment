58f897bf4b0f65981dad319ec8f7041e83d063c1
tariqul420 <tariqul@tariqul.dev>
2025-09-28 09:15:07 +0600

    Remove nvm and fnm configurations and completions from Fish shell

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,11 +1,16 @@
 # Oh My Posh prompt
-oh-my-posh init fish --config ~/.poshthemes/tokyonight_storm.omp.json | source
+oh-my-posh init fish --config ~/.poshthemes/custom-dev.omp.json | source
 
 if status is-interactive
-    # LSD aliases
-    alias ls="lsd --group-dirs=first --icon=always --color=always"
-    alias ll="lsd -lh --group-dirs=first --icon=always --color=always"
-    alias la="lsd -lha --group-dirs=first --icon=always --color=always"
+    # Enhanced ls with lsd (if available)
+    if command -q lsd
+        alias ls="lsd --group-dirs=first --icon=always --color=always"
+        alias ll="lsd -lh --group-dirs=first --icon=always --color=always"
+        alias la="lsd -lha --group-dirs=first --icon=always --color=always"
+    else
+        alias ll="ls -lh --color=auto"
+        alias la="ls -lha --color=auto"
+    end
 
     # Git shortcuts
     alias gs="git status"
@@ -13,12 +18,25 @@ if status is-interactive
     alias gc="git commit -m"
     alias gp="git push"
     alias gl="git pull"
+    alias gd="git diff"
+    alias gb="git branch"
+    alias gco="git checkout"
 
-    # Utils
-    alias cat="batcat --style=plain --paging=never"
+    # Enhanced utilities
+    if command -q batcat
+        alias cat="batcat --style=plain --paging=never"
+    else if command -q bat
+        alias cat="bat --style=plain --paging=never"
+    end
+    
+    # Navigation shortcuts
     alias cls="clear"
     alias ..="cd .."
     alias ...="cd ../.."
+    alias ....="cd ../../.."
+    
+    # Development shortcuts
+    alias c="code ."
+    alias npmls="npm list --depth=0"
+    alias npmg="npm list -g --depth=0"
 end
-
-set -Ux PATH $HOME/.local/share/fnm $PATH; fnm env --use-on-cd | source
